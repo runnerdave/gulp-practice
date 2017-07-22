d3e61c36ca70b895a7214c93e04197cfa3decf3c
runnerdave <runnerdave@gmail.com>
2017-07-22 12:03:35 +1000

    sass implementation

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,33 @@
-'use strict'
+'use strict';
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var livereload = require('gulp-livereload');
 var concat = require('gulp-concat');
-var minifyCss = require('gulp-minify-css');
 var autoprefixer = require('gulp-autoprefixer');
 var plumber = require('gulp-plumber');
 var sourcemaps = require('gulp-sourcemaps');
+var sass = require('gulp-sass');
 
 var SCRIPTS_PATH = 'public/scripts/**/*.js';
-var CSS_PATH = 'public/css/**/*.css';
 var DIST_PATH = 'public/dist';
 
-//Styles
+//Sass
 gulp.task('styles', function () {
     console.log('starting styles task');
-
-    return gulp.src(['public/css/reset.css', CSS_PATH])
-        .pipe(plumber(function(err) {
-            console.log('Styles task error!');
-            console.log(err)
+    return gulp.src('public/scss/styles.scss')
+        .pipe(plumber(function (err) {
+            console.log('Styles Task Error');
+            console.log(err);
             this.emit('end');
         }))
         .pipe(sourcemaps.init())
-    	.pipe(autoprefixer())
-        .pipe(concat('styles.css'))
-    	.pipe(minifyCss())
+        .pipe(autoprefixer())
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
         .pipe(sourcemaps.write())
-    	.pipe(gulp.dest(DIST_PATH))
-    	.pipe(livereload());
+        .pipe(gulp.dest(DIST_PATH))
+        .pipe(livereload());
 });
 
 //Scripts
@@ -52,7 +51,7 @@ gulp.task('watch', function () {
     require('./server.js');
     livereload.listen();
     gulp.watch(SCRIPTS_PATH, ['scripts']);
-    gulp.watch(CSS_PATH, ['styles'])
+    gulp.watch('public/scss/**/*.scss', ['styles'])
 });
 
 gulp.task('default', function () {
