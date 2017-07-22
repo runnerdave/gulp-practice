33064bbfba13bcb27928e933d5c8b0fcd6c57eaf
runnerdave <runnerdave@gmail.com>
2017-07-22 18:01:51 +1000

    delete files or folders

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var autoprefixer = require('gulp-autoprefixer');
 var plumber = require('gulp-plumber');
 var sourcemaps = require('gulp-sourcemaps');
 var babel = require('gulp-babel');
+var del = require('del');
 
 //handlebars plugins
 var handlebars = require('gulp-handlebars');
@@ -34,18 +35,18 @@ gulp.task('styles', function () {
     console.log('starting styles task');
 
     return gulp.src(['public/css/reset.css', CSS_PATH])
-        .pipe(plumber(function(err) {
+        .pipe(plumber(function (err) {
             console.log('Styles task error!');
             console.log(err);
             this.emit('end');
         }))
         .pipe(sourcemaps.init())
-    	.pipe(autoprefixer())
+        .pipe(autoprefixer())
         .pipe(concat('styles.css'))
-    	.pipe(minifyCss())
+        .pipe(minifyCss())
         .pipe(sourcemaps.write())
-    	.pipe(gulp.dest(DIST_PATH))
-    	.pipe(livereload());
+        .pipe(gulp.dest(DIST_PATH))
+        .pipe(livereload());
 });
 
 //Scripts
@@ -53,7 +54,7 @@ gulp.task('scripts', function () {
     console.log('starting scripts task');
 
     return gulp.src(SCRIPTS_PATH)
-        .pipe(plumber(function(err) {
+        .pipe(plumber(function (err) {
             console.log('Styles task error!');
             console.log(err);
             this.emit('end');
@@ -66,7 +67,7 @@ gulp.task('scripts', function () {
         .pipe(concat('scripts.js'))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest(DIST_PATH))
-    	.pipe(livereload());
+        .pipe(livereload());
 });
 
 // Images
@@ -111,6 +112,10 @@ gulp.task('watch', ['default'], function () {
     gulp.watch(TEMPLATES_PATH, ['templates']);
 });
 
-gulp.task('default', ['images', 'templates', 'styles', 'scripts'], function () {
+gulp.task('clean', function () {
+    return del.sync([DIST_PATH]);
+});
+
+gulp.task('default', ['clean', 'images', 'templates', 'styles', 'scripts'], function () {
     console.log('default task');
 });
